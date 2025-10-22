7b2b92d82cd2afe2394b38714ce16f84df7c4fe7
adithya16093 <tadiboyinaadithya2003@gmail.com>
2025-10-22 22:20:08 +0530

    Clean up Service.js by removing unused code
    Removed commented-out MySQL connection code.

--- back-end/Service.js
+++ back-end/Service.js
@@ -1,17 +1,4 @@
-// import mysql from "mysql2/promise";
- 
-// const pool = mysql.createPool({
-//   host: "localhost",     // ✅ only host, no jdbc
-//   port: 3306,            // ✅ optional, default is 3306
-//   user: "root",          // your MySQL username
-//   password: "Udaypratap@12",      // your MySQL password
-//   database: "sys",       // your database name
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
- 
-// export default pool;
+
 
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
